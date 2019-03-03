58dd8b4678e9b0abda744afa6b373592771045fa
Paul Ilves <pavlo.fomich@intellecteu.com>
2019-03-03 22:25:47 +0100

    deleted crap

--- scripts/specific.js
+++ scripts/specific.js
@@ -1,14 +1,10 @@
 "use strict";
-window.onload = run;
+window.onload = makeApiCall();
 
 // refer to question 2 before development starts for scope document
 // get URL query string
 function getQueryStringValue(key) {
     return decodeURIComponent(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + encodeURIComponent(key).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));
-}
-
-function run() {
-    makeApiCall();
 }
 
 function makeApiCall() {
