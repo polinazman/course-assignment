62c1a51eb777f1f5a3b9a5dc01872e3a2d73f3c5
Polina Z <polina.j4@gmail.com>
2019-03-03 13:30:48 +0500

    Update about.js

--- scripts/about.js
+++ scripts/about.js
@@ -1,1 +1,10 @@
 // refer to question 3 before development starts for scope document
+
+document.getElementById("aboutText").innerHTML = document.getElementById("aboutText").innerHTML.replace(/Magic/g, "Something");
+
+var moreInfo = document.getElementById("moreInfoTrigger");
+var infoContent = document.getElementById("moreInfoContent");
+moreInfo.addEventListener("click", function(e) {
+		infoContent.style.display = (infoContent.style.display === "none") ? "block" : "none";
+});
+
